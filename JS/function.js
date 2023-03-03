f0e090ca9dba20dd85da2a414b238556e3aadccf
unknown <diegoeolivera22@gmail.com>
2023-03-02 22:11:16 -0300

    Mi Primer Commit

--- JS/function.js
+++ JS/function.js
@@ -2,3 +2,29 @@ function showEmail() {
     var link = "mailto:email@ejemplo.com";
     window.location.href = link;
   }
+
+function mostrarHabilidades() {
+  var elemento = document.getElementById("listaHabilidades");
+  if (elemento.style.display === "none") {
+    elemento.style.display = "block";
+  } else {
+    elemento.style.display = "none";
+  }
+}function expandirInfo(id) {
+  var elemento = document.getElementById(id);
+  if (elemento.style.display === "none") {
+    elemento.style.display = "block";
+  } else {
+    elemento.style.display = "none";
+  }
+}
+function toggleEducacion() {
+  var educacion = document.getElementById("info-educacion");
+  if (educacion.style.display === "none") {
+    educacion.style.display = "block";
+    document.getElementById("toggle-educacion").innerHTML = "Esconder Educación";
+  } else {
+    educacion.style.display = "none";
+    document.getElementById("toggle-educacion").innerHTML = "Mostrar Educación";
+  }
+}
